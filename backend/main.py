1d5138ad1b7f7f733bc69790422949f5b66b90f6
John Louie Cleofas <164037152+JLCleofas@users.noreply.github.com>
2026-01-27 11:03:27 +0800

    Added theme toggle for dark mode

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import HTMLResponse
+from starlette.responses import Response
 
 from backend.middleware.log_middleware import log_middleware
 from backend.core.logger import logger
@@ -19,12 +19,30 @@ templates = Jinja2Templates(directory="backend/templates")
 
 @app.get("/")
 async def root(request: Request):
-    return  templates.TemplateResponse("base.html", {"request": request})
+    theme_color = request.cookies.get("theme")
+    return  templates.TemplateResponse("base.html", {"request": request, "theme_color": theme_color})
 
 @app.get("/home")
 async def home(request: Request):
     return templates.TemplateResponse("pages/home.html", {"request": request})
 
+@app.post("/theme")
+async def set_theme(request: Request, theme: str = Form(...)):
+    allowed = {"light", "dark"}
+    if theme not in allowed:
+        return Response(status_code=400, content={"message": "Invalid theme"})
+
+    resp = Response(status_code=204)
+
+    resp.set_cookie(key="theme",
+                    value=theme,
+                    max_age=86400,
+                    samesite="lax",
+                    secure=False, # set to True on PROD
+                    httponly=False,
+                    )
+
+    return resp
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
